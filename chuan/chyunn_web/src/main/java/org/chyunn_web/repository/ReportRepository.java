f61ce7e18459d0e40cda24a45005d44c435c6fc3
chuan025860 <142571934+chuan025860@users.noreply.github.com>
2025-06-05 17:33:29 +0800

    DELE

--- chuan/chyunn_web/src/main/java/org/chyunn_web/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package org.chyunn_web.repository;
-
-import org.chyunn_web.bean.incident.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Integer> {
-}
