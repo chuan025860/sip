a990c88f3f0d3e1172140fb8f0ba7ec428f5c95d
chuan025860 <142571934+chuan025860@users.noreply.github.com>
2025-06-13 17:25:41 +0800

    Del

--- chuan/chyunn_web/src/main/java/org/chyunn_web/repository/ReportFilesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.chyunn_web.repository;
-
-import org.chyunn_web.bean.incident.ReportFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportFilesRepository  extends JpaRepository<ReportFile,String> {
-}
